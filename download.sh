fc992e118fe647db299ea8ded0eeaf363743a858
Adi Shua <email@example.com>
2018-07-22 19:35:27 +0300

    Fixed download script Reset drupal installation  On branch master

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ./build.sh
 sleep 10
-docker exec drupal-8-example-web bash -c 'rm -rf /var/www/html/* && composer create-project drupal-composer/drupal-project:8.x-dev /var/www/html --stability dev --no-interaction'
+docker exec $CONTAINER_NAME_WEB bash -c 'rm -rf /var/www/html/* && composer create-project drupal-composer/drupal-project:8.x-dev /var/www/html --stability dev --no-interaction'
