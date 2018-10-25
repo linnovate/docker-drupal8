24904b3638462f9910134cfcff3f1fe4ff4253cc
Adi Shua <email@example.com>
2018-10-25 12:22:50 +0300

    moved drupal to a dedicated folder in order to integrate custom functionallity  On branch master

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 ./build.sh
 sleep 10
 source .env
-docker exec $CONTAINER_NAME_WEB bash -c 'rm -rf /var/www/html/web && composer create-project drupal-composer/drupal-project:8.x-dev /var/www/html --stability dev --no-interaction'
-docker exec $CONTAINER_NAME_WEB bash -c 'drush site-install --db-url=mysql://root:newstart@db/db_name -y'
+docker exec $CONTAINER_NAME_WEB bash -c 'mkdir drupal && cd drupal && composer create-project drupal-composer/drupal-project:8.x-dev /var/www/html/drupal --stability dev --no-interaction'
+docker exec $CONTAINER_NAME_WEB bash -c 'drush site-install --db-url=mysql://root:password@db/db_name -y'
