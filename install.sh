bdb43d556e723bfc73563d9b56b5a95e0a8678fe
aron <astein@linnovate.net>
2018-11-13 17:19:40 +0200

    add composer.json

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 ./build.sh
 sleep 10
 source .env
-docker exec $CONTAINER_NAME_WEB bash -c 'mkdir drupal && cd drupal && composer create-project drupal-composer/drupal-project:8.x-dev /var/www/html/drupal --stability dev --no-interaction'
+docker exec $CONTAINER_NAME_WEB bash -c 'export COMPOSER_ALLOW_SUPERUSER=1; composer install --no-dev'
 docker exec $CONTAINER_NAME_WEB bash -c 'drush site-install --db-url=mysql://root:password@db/db_name -y'
