60ff55490cb1cd2c559a8fdf450f3f01341e2b1b
aron <astein@linnovate.net>
2018-11-14 11:02:31 +0200

    add mkdir

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ sleep 10
 source .env
 docker exec $CONTAINER_NAME_WEB bash -c 'export COMPOSER_ALLOW_SUPERUSER=1; composer install --no-dev'
 docker exec $CONTAINER_NAME_WEB bash -c 'drush site-install --db-url=mysql://root:password@db/db_name -y'
+
+docker exec $CONTAINER_NAME_WEB bash -c 'mkdir modules themes && chmod 777 ./modules ./themes -R'
+
